2e7b7728b17dc7c0fd14c8bc37c165c50391487b
nualagandon <67584134+nualagandon@users.noreply.github.com>
2025-02-03 15:18:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const xhr = new XMLHttpRequest();
+document.querySelectorAll("button").addEventListener("click", function() {
+    const xhr = new XMLHttpRequest();
 
-const HttpMethod = "get";
-const url = ""
+    const HttpMethod = "get";
+    const url = "https://api.tisseo.fr/v2/lines.json?key=a3732a1074e2403ce364ad6e71eb998cb";
+    xhr.open(HttpMethod, url);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const reponse = JSON.parse(xhr.responseText);
+        }
+    }
+
+    xhr.send();
+});
+
